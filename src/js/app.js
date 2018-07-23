bf7a20dbdd0dcc19de1125b36de198066e48f7a7
avanger <den2512@ukr.net>
2018-07-23 19:11:17 +0300

    build

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,9 @@
-import objectFitImages from 'object-fit-images';
 import svg4everybody from 'svg4everybody';
 import './components.js';
 
 
 
 $(function() {
-  objectFitImages();
 
   svg4everybody({
     nosvg: true,
